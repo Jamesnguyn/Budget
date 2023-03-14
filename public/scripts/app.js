5b6a77d0c3916efca2ae6533b1ee19bf34739d6e
James Nguyen <jameztnguyen@hotmail.com>
2023-03-14 13:50:45 -0700

    jsx expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,77 @@
-"use strict";
+'use strict';
 
 console.log("App js running");
 
+var app = {
+  title: 'Indecision App',
+  subTitle: 'paragraph'
+};
+
 var template = React.createElement(
-  "h1",
+  'div',
   null,
-  "Indecision App"
+  React.createElement(
+    'h1',
+    null,
+    ' ',
+    app.title,
+    ' '
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' ',
+    app.subTitle,
+    ' '
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      ' one '
+    ),
+    React.createElement(
+      'li',
+      null,
+      ' two '
+    )
+  )
 );
+
+var user = {
+  name: 'James',
+  age: '25',
+  location: 'San Diego'
+};
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    ' ',
+    user.name,
+    ' '
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' Age: ',
+    user.age,
+    ' '
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' Location: ',
+    user.location,
+    ' '
+  )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,36 @@
 console.log("App js running");
 
-var template = <h1>Indecision App</h1>;
+var app = {
+  title: 'Indecision App',
+  subTitle: 'paragraph',
+};
+
+var template = (
+  <div> 
+    <h1> {app.title} </h1> 
+    <p> {app.subTitle} </p> 
+    <ol>
+      <li> one </li>
+      <li> two </li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'James',
+  age: '25',
+  location: 'San Diego'
+};
+
+var template2 = (
+  <div> 
+    <h1> {user.name} </h1> 
+    <p> Age: {user.age} </p>
+    <p> Location: {user.location} </p> 
+  </div>
+);
+
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
