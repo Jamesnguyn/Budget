b2c6494375ef037896fc1cffc3c379c7e1d523d3
James Nguyen <jameztnguyen@hotmail.com>
2023-03-28 22:26:44 -0700

    import npm modules

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
-// import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(4, 4));
-// console.log(subtract(4, 4));
+const template = React.createElement('p', {}, 'TESTING');
 
-import isSenior, { isAdult, canDrink } from "./person";
-
-let x = 21;
-console.log(isAdult(x));
-console.log(canDrink(x));
-console.log(isSenior(x));
+ReactDOM.render(template, document.getElementById('app'));
