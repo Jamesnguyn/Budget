e5e368252cf299ef51ffad01daf36b1d73005c4d
James Nguyen <jameztnguyen@hotmail.com>
2023-04-06 22:58:49 -0700

    query strings and url params

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    This is from my edit expense component
-  </div>
-);
+const EditExpensePage = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      Editing {props.match.params.id}
+      This is from my edit expense component
+    </div>
+  );
+};
 
 export default EditExpensePage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,7 @@ const Header = () => (
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>dashboard </NavLink>
     <NavLink to="/create" activeClassName="is-active">add expense </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">edit expense </NavLink>
-    <NavLink to="/help" activeClassName="is-active">help page </NavLink>
+    <NavLink to="/help" activeClassName="is-active">help </NavLink>
   </header>
 );
 
