eb5247bab7b9d9c495f77318576bab7e3f0b84a1
James Nguyen <jameztnguyen@hotmail.com>
2023-04-12 02:17:01 -0700

    refactoring and organizing

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -12,14 +12,27 @@
 
 // console.log(`${name} is ${age} he lives in ${city} and it is ${temp} degrees`);
 
-const book = {
-  title: 'ABC',
-  author: 'JRR Tolkien',
-  publisher: {
-    name: 'LOTR',
-  }
-};
+// const book = {
+//   title: 'ABC',
+//   author: 'JRR Tolkien',
+//   publisher: {
+//     name: 'LOTR',
+//   }
+// };
 
-const { name: publisherName = 'self-published' } = book.publisher;
+// const { name: publisherName = 'self-published' } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+//ARRAY DESTRUCTURING
+
+const address = ['1234 Street Street', 'San Diego', 'California', '92126'];
+
+const [street, city, state, zip] = address;
+
+console.log(` you are in ${city} ${state}`);
+
+const item = ['coffee (hot)', ' $2', '$2.5', '$2.75'];
+const [coffee, small, medium, large] = item;
+
+console.log(`a medium ${coffee} costs ${medium}`);
