ecbfda28e8fe7509b43a4ffc8c0a55bc224aabca
James Nguyen <jameztnguyen@hotmail.com>
2023-05-11 11:37:31 -0700

    creating separate database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyCdsHMahPCxRp9tB4zbT9DfKqXN4H0H-KY",
-  authDomain: "budgetapp-f017c.firebaseapp.com",
-  databaseURL: "https://budgetapp-f017c-default-rtdb.firebaseio.com",
-  projectId: "budgetapp-f017c",
-  storageBucket: "budgetapp-f017c.appspot.com",
-  messagingSenderId: "864075930066",
-  appId: "1:864075930066:web:e4fd2e7622985b678cd00b",
-  measurementId: "G-MQLF20Z4ZK"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
