e4586a304e03bc62d68fa3acb3b3897ba75a9d4c
James Nguyen <jameztnguyen@hotmail.com>
2023-05-22 21:12:44 -0700

    logging out

--- src/tests/components/LoginPage.js
+++ src/tests/components/LoginPage.js
@@ -6,3 +6,10 @@ test('render login page', () => {
   const wrapper = shallow(<LoginPage />);
   expect(wrapper).toMatchSnapshot();
 })
+
+test('call startlogin on button click', () => {
+  const startLogin = jest.fn();
+  const wrapper = shallow(<LoginPage startLogin={startLogin}/>);
+  wrapper.find('button').simulate('click');
+  expect(startLogin).toHaveBeenCalled();
+});
