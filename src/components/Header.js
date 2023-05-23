0c73548072f1d974c46a553d6cc370fdc00d50cf
James Nguyen <jameztnguyen@hotmail.com>
2023-05-22 21:53:30 -0700

    redirect login and logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { startLogout } from '../actions/auth';
 export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>dashboard </NavLink>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">add expense </NavLink>
     <NavLink to="/help" activeClassName="is-active">help </NavLink>
     <button onClick={startLogout}>Logout</button>
